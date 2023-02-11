8a1675def1378f6ffd9b09cf523985b1068a3f30
Francois973 <robino.francois973@gmail.com>
2023-02-11 10:46:29 +0100

    chore: remove old value in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,6 @@ room_8a = Room.create(name: 'Room8a')
 room_8f = Room.create(name: 'Room8f')
 room_b2 = Room.create(name: 'Roomb2')
 
-# ROOMS = [room_8a, room_8f, room_b2].freeze
-# FILES = ['room_8a.csv', 'room_8f.csv', 'room_b2.csv'].freeze
-
 def insert_datas_database(file, room)
   csv_text = File.read(Rails.root.join('lib', 'seeds', file))
   csv = CSV.parse(csv_text, headers: true)
@@ -42,44 +39,3 @@ end
 insert_datas_database('room_8a.csv', room_8a)
 insert_datas_database('room_8f.csv', room_8f)
 insert_datas_database('room_b2.csv', room_b2)
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'room_8a.csv'))
-# csv = CSV.parse(csv_text, headers: true)
-
-# csv.each do |measure|
-#   m = Measure.new
-#   m.co2 =  measure['measure_float'] if measure['measure_type'] == 'CO2'
-#   m.hum =  measure['measure_float'] if measure['measure_type'] == 'HUM'
-#   m.tmp =  measure['measure_float'] if measure['measure_type'] == 'TMP'
-#   m.voct = measure['measure_float'] if measure['measure_type'] == 'VOCT'
-#   m.timestamp = measure['@timestamp']
-#   m.room_id = @room_8a.id
-#   m.save
-# end
-
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'room_8f.csv'))
-# csv = CSV.parse(csv_text, headers: true)
-
-# csv.each do |measure|
-#   m = Measure.new
-#   m.co2 =  measure['measure_float'] if measure['measure_type'] == 'CO2'
-#   m.hum =  measure['measure_float'] if measure['measure_type'] == 'HUM'
-#   m.tmp =  measure['measure_float'] if measure['measure_type'] == 'TMP'
-#   m.voct = measure['measure_float'] if measure['measure_type'] == 'VOCT'
-#   m.timestamp = measure['@timestamp']
-#   m.room_id = @room_8f.id
-#   m.save
-# end
-
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'room_8f.csv'))
-# csv = CSV.parse(csv_text, headers: true)
-
-# csv.each do |measure|
-#   m = Measure.new
-#   m.co2 =  measure['measure_float'] if measure['measure_type'] == 'CO2'
-#   m.hum =  measure['measure_float'] if measure['measure_type'] == 'HUM'
-#   m.tmp =  measure['measure_float'] if measure['measure_type'] == 'TMP'
-#   m.voct = measure['measure_float'] if measure['measure_type'] == 'VOCT'
-#   m.timestamp = measure['@timestamp']
-#   m.room_id = @room_b2.id
-#   m.save
-# end
