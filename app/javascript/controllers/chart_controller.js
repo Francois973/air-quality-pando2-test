d52ae83989a2af61a66d22a753149e7b49d32b5d
Francois973 <robino.francois973@gmail.com>
2023-02-11 17:18:20 +0100

    chore: clean chart_controller and ready to create two other charts

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -8,16 +8,6 @@ export default class extends Controller {
 
   connect() {
     console.log("--->");
-
-    // const room8aCtx = this.room8aTarget.getContext("2d");
-
-    // const room8aTimestamps = this.fetchDataTimestamps(this.room8aTarget);
-
-    // const room8aCo2 = this.fetchDataCo2(this.room8aTarget);
-    // const room8aHum = this.fetchDataHum(this.room8aTarget);
-    // const room8aTmp = this.fetchDataTmp(this.room8aTarget);
-    // const room8aVoct = this.fetchDataVoct(this.room8aTarget);
-
     this.fetchData();
     this.displayChart();
   }
@@ -68,31 +58,4 @@ export default class extends Controller {
       },
     });
   }
-
-  // fetchDataTimestamps(target) {
-  //   const datas = JSON.parse(target.getAttribute("data-chart-value"));
-  //   console.log(datas);
-  //   return (this._timestamp =
-  //     this._timestamp || datas.map((measure) => measure.timestamp));
-  // }
-
-  // fetchDataCo2(target) {
-  //   const datas = JSON.parse(target.getAttribute("data-chart-value"));
-  //   return (this._co2 = this._co2 || datas.map((measure) => measure.co2));
-  // }
-
-  // fetchDataHum(target) {
-  //   const datas = JSON.parse(target.getAttribute("data-chart-value"));
-  //   return (this._hum = this._hum || datas.map((measure) => measure.hum));
-  // }
-
-  // fetchDataTmp(target) {
-  //   const datas = JSON.parse(target.getAttribute("data-chart-value"));
-  //   return (this._tump = this._tmp || datas.map((measure) => measure.tmp));
-  // }
-
-  // fetchDataVoct(target) {
-  //   const datas = JSON.parse(target.getAttribute("data-chart-value"));
-  //   return (this._voct = this._voct || datas.map((measure) => measure.voct));
-  // }
 }
