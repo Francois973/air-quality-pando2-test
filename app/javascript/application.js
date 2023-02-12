46986e61df3c653c89bf1da69f721e53e22ea980
Francois973 <robino.francois973@gmail.com>
2023-02-12 16:08:08 +0100

    chore: fix scales proportion of each gaz and add titles on axis

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 import "chart.js";
+import "moment";
+import "chartjs-adapter-moment";
--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -1,13 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
-
 import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
+import "chartjs-adapter-moment";
 
+// console.log(moment);
 export default class extends Controller {
   static targets = ["room8a", "room8f", "roomb2"];
 
   connect() {
-    console.log("--->");
+    console.log("------>");
     this.fetchData();
     this.displayChart();
   }
@@ -25,36 +26,111 @@ export default class extends Controller {
 
   displayChart() {
     const room8aCtx = this.room8aTarget.getContext("2d");
-    const room8aChart = new Chart(room8aCtx, {
+    return new Chart(room8aCtx, {
       type: "line",
       data: {
         labels: this.timestamps,
+        datasets: {
+          data: this.timestamps,
+          xAxisID: "xAxis",
+        },
         datasets: [
           {
             label: "CO2",
             data: this.co2,
             borderColor: "#3e95cd",
             fill: false,
+            yAxisID: "co2",
           },
           {
             label: "HUM",
             data: this.hum,
             borderColor: "#8e5ea2",
             fill: false,
+            yAxisID: "hum",
           },
           {
             label: "TMP",
             data: this.tmp,
             borderColor: "#3cba9f",
             fill: false,
+            yAxisID: "tmp",
           },
           {
-            label: "VOC",
+            label: "VOCT",
             data: this.voct,
             borderColor: "#e8c3b9",
             fill: false,
+            yAxisID: "voct",
           },
         ],
+      },
+      options: {
+        scales: {
+          co2: {
+            title: {
+              display: true,
+              text: "Co2",
+              color: "#3e95cd",
+              position: "top",
+            },
+            type: "linear",
+            position: "left",
+            min: 200,
+            max: 460,
+          },
+          hum: {
+            type: "linear",
+            position: "left",
+            min: 0,
+            max: 100,
+            title: {
+              display: true,
+              text: "Hum",
+              color: "#8e5ea2",
+              position: "top",
+            },
+          },
+          tmp: {
+            type: "linear",
+            position: "right",
+            min: 0,
+            max: 20,
+            title: {
+              display: true,
+              text: "Tmp",
+              color: "#3cba9f",
+              position: "top",
+            },
+          },
+          voct: {
+            type: "linear",
+            position: "right",
+            min: 0,
+            max: 300,
+            title: {
+              display: true,
+              text: "Voct",
+              color: "#e8c3b9",
+              position: "top",
+            },
+          },
+          xAxis: {
+            type: "time",
+            time: {
+              unit: "minute",
+              displayFormats: {
+                minute: "mm",
+              },
+            },
+            display: false,
+            distribution: "linear",
+            position: "bottom",
+            gridLines: {
+              display: false,
+            },
+          },
+        },
       },
     });
   }
