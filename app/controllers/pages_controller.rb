5ca582aefe91118a4a70945ed018a560b9e2d603
Francois973 <robino.francois973@gmail.com>
2023-02-13 16:25:51 +0100

    chore: prepare page controller to send datas formated to home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,35 @@
 class PagesController < ApplicationController
-  def home; end
+  def home
+    measures = Measure.order(measured_at: :asc)
+    rooms = measures.pluck(:room).uniq
+
+    measure_types = format_datas(measures, rooms)
+
+    @datas = fill_datas_measures(measure_types, rooms, measures)
+  end
+
+  private
+
+  def fill_datas_measures(measure_types, rooms, measures)
+    measures.each_with_object(measure_types) do |measure_db, acc|
+      currency_measure = acc[measure_db.category]
+
+      currency_measure['labels'] = [] unless currency_measure['labels']
+      currency_measure['labels'] << measure_db.measured_at
+      ap rooms
+      rooms.each do |room|
+        currency_measure[room] << measure_db.value if measure_db.room == room
+        currency_measure[room] << nil unless measure_db.room == room
+      end
+
+      acc[measure_db.category] = currency_measure
+      acc
+    end
+  end
+
+  def format_datas(measures, rooms)
+    measures.pluck(:category).uniq.each_with_object({}) do |measure_type, acc|
+      acc[measure_type] = rooms.each_with_object({}) { |room, acc2| acc2[room] = [] }
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,9 @@ require 'csv'
 
 Measure.destroy_all
 
-["./room_8a.csv", "./room_8f.csv", "./room_b2.csv"].each do |file|
+['./room_8a.csv', './room_8f.csv', './room_b2.csv'].each do |file|
   CSV.foreach(file, headers: true) do |row|
-    Measure.create(room: row["room_name"], category: row["measure_type"], value: row["measure_float"], measured_at: row["@timestamp"])
+    Measure.create(room: row['room_name'], category: row['measure_type'], value: row['measure_float'],
+                   measured_at: row['@timestamp'])
   end
 end
