52271e396de706fcdea8a6ea2e5a09a019602660
Francois973 <robino.francois973@gmail.com>
2023-02-14 09:54:28 +0100

    chore: add test in pages controller for get home page with params

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,10 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test 'should get home with parameters' do
+    get root_url, params: { rooms: 'Room 8A', measures: 'CO2' }
+
+    assert_response :success
+  end
 end
